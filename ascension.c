effaa589e543273fbaa2bb5689a57a92e7d9ef91
norisak <23559545+norisak@users.noreply.github.com>
2017-11-11 22:25:33 +0100

    Added ascension.c and ascension_processor.py

---
+++ ascension.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define NUM_SIGNETS 6
+#define NUM_ATTEMPTS 10000
+#define NUM_CROSSBOWS 1
+// Uncomment below to show debug print messages
+//#define DEBUG 1 
+
+
+int min(int *array, size_t size){
+	int m=array[0];
+
+	for (int i = 1; i < size; ++i){
+		if (array[i] < m){
+			m = array[i];
+		}
+	}
+	return m;
+}
+
+
+int roll_keystone(){
+	// Assumes that rand() returns 15 random bits
+	return (rand() >> 9) == 0;  // Has a 1/64 chance of returning 1
+}
+
+
+int roll_signet(){
+	return (rand() % 50) == 0;  // Has a ~1/50 chance of returning 1
+}
+
+
+int rand_signet(){
+	return rand() % NUM_SIGNETS;
+}
+
+
+void print_array(int* array, size_t size){
+	for(int i = 0; i < size; ++i){
+		printf("%d ",array[i]);
+	}
+	printf("\n");
+}
+
+
+void reset(int **kc, int **signet_counts){
+	free(*kc);
+	free(*signet_counts);
+	*signet_counts = calloc(NUM_SIGNETS, sizeof(int));
+	*kc = calloc(1, sizeof(int));
+}
+
+
+void get_ascension_crossbow(int *kc, int *signet_counts){
+	// Set the required signets to one more than what we have the least of. 
+	// This will allow us to create one more crossbow.
+	int required_signets = min(signet_counts, NUM_SIGNETS) + 1;
+	while (1){
+		// Kill an ascension creature, and roll for a keystone
+		*kc += 1;
+		if (roll_keystone()){
+			// Pick a random signet/keystone
+			int signet_index = rand_signet();
+
+			// Kill a Legio and see if we get a signet
+			if (roll_signet()){
+				signet_counts[signet_index]+= 1;
+				
+				// Check if we have enough signets to make a crossbow
+				if (min(signet_counts, NUM_SIGNETS) >= required_signets){
+					#ifdef DEBUG
+						printf("Done at %d as we got a keystone_index = %d\n",*kc, signet_index);
+						printf("Signet counts: ");
+						print_array(signet_counts,NUM_SIGNETS);
+					#endif
+					return;
+				}
+			}
+		}
+	}
+}
+
+
+int main(){
+
+	int *signet_counts = calloc(NUM_SIGNETS, sizeof(int));
+	int *kc = calloc(1, sizeof(int));
+
+	long long total_kc;
+
+	for (int i = 0; i < NUM_ATTEMPTS; ++i){
+		// Simulate killing ascension creatures until we get the desired amount of crossbows
+		for (int j = 0; j < NUM_CROSSBOWS; ++j){
+			get_ascension_crossbow(kc, signet_counts);
+		}
+		total_kc += *kc;
+		// Print the kc
+		printf("%d\n",*kc);
+		// Reset the kc and amount of signets for the next attempt.
+		reset(&kc, &signet_counts);
+	}
+
+	free(signet_counts);
+	free(kc);
+}
---
+++ ascension_processor.py
@@ -0,0 +1,56 @@
+from sys import stdin
+from math import lgamma, exp, log
+
+
+def get_num_lt_or_eq(l, num):
+    """
+
+    :param l: The list we are searching
+    :param num: Upper bound
+    :return: The number of elements in l that are less than or equal to num.
+    """
+    x = 0
+    for kc in l:
+        if kc <= num:
+            x += 1
+    return x
+
+
+def get_percentile(l, p):
+    """
+
+    :param l: The list we are searching
+    :param p: The percentile we want to find. Must be in the interval [0, 1)
+    :return: The element at position p*listsize
+    """
+    position = int(len(l) * p)
+    return l[position]
+
+
+def main():
+    kc_history = []
+    # read the killcount data from the input file and put it in kc_history
+    for line in stdin:
+        num = int(line)
+        kc_history.append(num)
+
+    # sort the data to make it easier to work with
+    kc_history.sort()
+    print("Number of data points: {}".format(len(kc_history)))
+    print("Killcount - # of simulations having completed the crossbows at this killcount")
+
+    average = sum(kc_history) // len(kc_history)
+
+    for x in range(250):
+        x = x * 1000
+        print("{}\t{}".format(x, get_num_lt_or_eq(kc_history, x)))
+    
+    print("1% Luckiest: {}".format(get_percentile(kc_history, 0.01)))
+    print("5% Luckiest: {}".format(get_percentile(kc_history, 0.05)))
+    print("50% Median: {}".format(get_percentile(kc_history, 0.5)))
+    print("Average {}".format(average))
+    print("5% Unluckiest: {}".format(get_percentile(kc_history, 0.95)))
+    print("1% Unluckiest: {}".format(get_percentile(kc_history, 0.99)))
+
+
+main()
